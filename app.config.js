8fc45a79566fe7cb9c7e52d4e24be34587d19dd4
Tracy Nyamnjoh <tracynyamnjoh@gmail.com>
2025-12-30 13:47:09 -0500

    Build

--- app.config.js
+++ app.config.js
@@ -8,6 +8,10 @@ export default {
     extra: {
       MAPBOX_TOKEN: process.env.EXPO_PUBLIC_MAPBOX_TOKEN,
       GOOGLE_API_KEY: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+
+      eas: {
+        projectId: "93531cff-0dea-4972-afdb-592e5774b84e",
+      },
     },
   },
 };
