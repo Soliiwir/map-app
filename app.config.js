6dd70c0cadd4cb24ac1f48ad3f2e3c0447b23f93
Tracy Nyamnjoh <tracynyamnjoh@gmail.com>
2025-12-30 17:05:48 -0500

    Build

--- app.config.js
+++ app.config.js
@@ -4,13 +4,71 @@ export default {
   expo: {
     name: "Map-app",
     slug: "map-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
     scheme: "mapapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.mapapp",
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission: "Allow App to access your location",
+        },
+      ],
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: { backgroundColor: "#000000" },
+        },
+      ],
+      "expo-font",
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
     extra: {
       MAPBOX_TOKEN: process.env.EXPO_PUBLIC_MAPBOX_TOKEN,
       GOOGLE_API_KEY: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
-
       eas: {
-        projectId: "93531cff-0dea-4972-afdb-592e5774b84e",
+        projectId: "93531cff-0dea-4972-afdb-592e5774b84e", // your EAS projectId
+      },
+      router: {},
+    },
+    updates: {
+      url: "https://u.expo.dev/93531cff-0dea-4972-afdb-592e5774b84e",
+      android: {
+        runtimeVersion: "1.0.0",
+      },
+      ios: {
+        runtimeVersion: {
+          policy: "appVersion",
+        },
       },
     },
   },
